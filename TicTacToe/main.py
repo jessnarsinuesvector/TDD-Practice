6fa7dfa49b91024f365e96872d17a4a04c81135e
Jessnar Sinues <jessnar.sinues@vector.co.nz>
2024-07-31 08:43:02 +1200

    TicTacToe: Added sys append in main.py

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -2,6 +2,7 @@
 # https://github.com/ParthJadhav/Tkinter-Designer
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 from TicTacToe.src.input_controller import InputController
 from TicTacToe.src.result_checker import ResultChecker
